e90ccdd955abfa52f9944067a86b7fc028916d4b
--global <sshb1712@gmailcom>
2021-04-14 17:08:03 +0500

    version1

---
+++ script.js
@@ -0,0 +1,20 @@
+var ism = prompt('Sizning ismingiz?')
+console.log('Mening ismim ' + ism); 
+
+var yosh = prompt('Sizning ismingiz?')
+console.log('Men ' + yosh + ' yoshdaman');
+
+var misol1 = prompt('Misolni yeching: 5 + 7 = ')
+console.log('5 + 7 = 12 ' + 'Sizning javob ' + misol1);
+
+var misol1 = prompt('Misolni yeching: 10 - 6 = ')
+console.log('10 - 6 = 4 ' + 'Sizning javob ' + misol1);
+
+var misol1 = prompt('Misolni yeching: 5 * 4 = ')
+console.log('5 * 4 = 20 ' + 'Sizning javob ' + misol1);
+
+var misol1 = prompt('Misolni yeching: 50 / 10 = ')
+console.log('50 / 10 = 5 ' + 'Sizning javob ' + misol1);
+
+var misol1 = prompt('Misolni yeching: 22 % 7 = ')
+console.log('22 % 7 = 1 ' + 'Sizning javob ' + misol1);
